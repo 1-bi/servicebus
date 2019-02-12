ad3fb36a02c60bb2774f76d5eca19fad2777193f
Vicente Yuen <vison_ruan@126.com>
2019-02-12 23:23:27 +0800

    update progress

--- encoder/gencode.go
+++ encoder/gencode.go
@@ -1,5 +1,15 @@
 package encoder
 
+import "github.com/1-bi/servicebus/schema"
+
 // GencodeEncoder define gencode encoder
 type GencodeEncoder struct {
+}
+
+func (myself *GencodeEncoder) Encode(reqMsg *schema.ReqMsg) ([]byte, error) {
+	return nil, nil
+}
+
+func (myself *GencodeEncoder) Decode(inputContent []byte, resultObj interface{}) {
+
 }
